7aa9fb9d2d921fc02c9a2e324fb530b5a3476988
dennis mwaniki <dennomaz@gmail.com>
2022-11-19 13:03:13 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,61 @@
+let number = 0
+
+function add() {
+    return (number+1);
+};
+
+function subtract(){
+    return (number-1);
+};
+
+function multiply(){
+    return (number*2);
+};
+
+function divide(){
+    return (number/2);
+};
+
+let a = 0;
+let b = 0;
+
+
+function add(a,b){
+    return (a+b);
+};
+function subtract(a,b){
+    return (a-b);
+};
+function multiply(a,b){
+    return (a*b);
+};
+function divide(a,b){
+    return (a/b);
+};
+
+let n = 0;
+
+function increment(n){
+    return ++n;
+};
+
+function decrement(n){
+    return --n;
+};
+
+function makeInt(a,b){
+    return (parseInt("2.43",10));
+}
+
+function makeInt(n){
+    return (parseInt(n,10))
+} ;
+
+function preserveDecimal(n){
+    return (parseFloat(n))
+}
+
+
+
+
+
